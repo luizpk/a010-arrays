c86fbf1d894f6855d6c32694e41446c3a896e3d2
LuizPK <luizpkpinheiro@gmail.com>
2022-09-01 10:51:29 -0300

    Executando exercicio1

---
+++ exercicio1/script.js
@@ -0,0 +1,20 @@
+//No primeiro exercício vamos praticar a criação de arrays. Crie 3 arrays diferentes:
+
+
+
+//- O primeiro deve possuir apenas **números**, e estes números não devem seguir nenhuma ordem específica;
+
+const arrayNumeros = [25,69,250,986,74]
+
+//- O segundo array deve possuir apenas **strings**;
+
+const arrayString = ["lasanha", "banana","alcachofra","queijo"]
+
+//- O terceiro array deve possuir **números**, **strings** e **booleanos**.
+const arrayMisturado = [256,"banana",5>2, "cachorro", 4+5===9,682]
+
+//Imprima cada um dos arrays acima, utilizando `console.log()`
+
+console.log(`${arrayNumeros}
+${arrayString}
+${arrayMisturado}`)
